1ce67acbb8b60a6411e01c0d4fb5410106b9f969
Ricardo SIGNES <rjbs@codesimply.com>
2006-11-17 00:29:53 +0000

    r28194@knight:  rjbs | 2006-11-16 19:28:21 -0500  release-prep

--- lib/App/Cmd.pm
+++ lib/App/Cmd.pm
@@ -12,13 +12,13 @@ App::Cmd - write command line apps with less suffering
 
 =head1 VERSION
 
-version 0.007
+version 0.008
 
  $Id$
 
 =cut
 
-our $VERSION = '0.007';
+our $VERSION = '0.008';
 
 =head1 SYNOPSIS
 
